00613f0eb0d4dec316c2e986b62f962bf5f52160
Amane Misa <mansion.pe@gmail.com>
2024-01-26 01:34:07 -0500

    delete suspense and loader styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from "react";
+import { lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Home = lazy(() => import("./pages/Home"));
@@ -13,37 +13,20 @@ const NotFound = lazy(() => import("./pages/NotFound"));
 function App() {
   return (
     <Router>
-      <Suspense
-        fallback={
-          <div className="lds-wrapper">
-            <div className="lds-roller">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-        }
-      >
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/post/:id" element={<Post />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
-          <Route exact path="/perfil" element={<Profile />}></Route>
-          <Route exact path="/perfil/post" element={<CreatePost />}></Route>
-          <Route
-            exact
-            path="/perfil/post/editar"
-            element={<UpdatePost />}
-          ></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/post/:id" element={<Post />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/register" element={<Register />}></Route>
+        <Route exact path="/perfil" element={<Profile />}></Route>
+        <Route exact path="/perfil/post" element={<CreatePost />}></Route>
+        <Route
+          exact
+          path="/perfil/post/editar"
+          element={<UpdatePost />}
+        ></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </Router>
   );
 }
