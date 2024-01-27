6f6a79f1ed25ea4d85ae30f8507c041efe83fecf
Amane Misa <mansion.pe@gmail.com>
2024-01-26 20:37:52 -0500

    restore suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -13,20 +13,37 @@ const NotFound = lazy(() => import("./pages/NotFound"));
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/post/:id" element={<Post />}></Route>
-        <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/register" element={<Register />}></Route>
-        <Route exact path="/perfil" element={<Profile />}></Route>
-        <Route exact path="/perfil/post" element={<CreatePost />}></Route>
-        <Route
-          exact
-          path="/perfil/post/editar"
-          element={<UpdatePost />}
-        ></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="lds-wrapper">
+            <div className="lds-roller">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/post/:id" element={<Post />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/perfil" element={<Profile />}></Route>
+          <Route exact path="/perfil/post" element={<CreatePost />}></Route>
+          <Route
+            exact
+            path="/perfil/post/editar"
+            element={<UpdatePost />}
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
