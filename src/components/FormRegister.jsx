5282946031fa541f817c5b22184771afdf845949
Amane Misa <mansion.pe@gmail.com>
2024-02-11 01:37:56 -0500

    improve register flow

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import confetti from "canvas-confetti";
 
 import "./styles/FormRegister.scss";
 import "./styles/FormButton.scss";
@@ -10,6 +11,22 @@ export const FormRegister = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [successRegistration, setSuccessRegistration] = useState(false);
+
+  // Inicializacion de Funcion Confetti
+  var count = 200;
+  var defaults = {
+    origin: { y: 0.7 },
+  };
+
+  function fire(particleRatio, opts) {
+    confetti({
+      ...defaults,
+      ...opts,
+      particleCount: Math.floor(count * particleRatio),
+    });
+  }
+  // Fin de Funcion Confetti
 
   const handleChange = (e) => {
     setRegisterHasError(false);
@@ -87,7 +104,32 @@ export const FormRegister = () => {
         .then((response) => response.json())
         .then((data) => {
           if (data.status === 201) {
-            window.location.href = "/login";
+            fire(0.25, {
+              spread: 26,
+              startVelocity: 55,
+            });
+            fire(0.2, {
+              spread: 60,
+            });
+            fire(0.35, {
+              spread: 100,
+              decay: 0.91,
+              scalar: 0.8,
+            });
+            fire(0.1, {
+              spread: 120,
+              startVelocity: 25,
+              decay: 0.92,
+              scalar: 1.2,
+            });
+            fire(0.1, {
+              spread: 120,
+              startVelocity: 45,
+            });
+            setSuccessRegistration(true);
+            setTimeout(() => {
+              window.location.href = "/login";
+            }, 5000);
           } else if (data.status === 401) {
             console.log(data.message);
             setEmailIsTaken(true);
@@ -112,53 +154,65 @@ export const FormRegister = () => {
         </nav>
         <main>
           <div className="auth-content">
-            <h2 className="form-title">Registro</h2>
-            <form onSubmit={handleSubmit}>
-              <fieldset>
-                <label htmlFor="name">Nombre</label>
-                <input
-                  onChange={handleChange}
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={name}
-                />
-              </fieldset>
-              <fieldset>
-                <label htmlFor="email">Email</label>
-                <input
-                  onChange={handleChange}
-                  type="text"
-                  id="email"
-                  name="email"
-                  value={email}
-                />
-              </fieldset>
-              <fieldset>
-                <label htmlFor="password">Contraseña</label>
-                <input
-                  onChange={handleChange}
-                  type="password"
-                  id="password"
-                  placeholder="6+ caracteres"
-                  name="password"
-                  value={password}
-                />
-              </fieldset>
-              <fieldset>
-                {registerHasError && (
-                  <label className="text-red-600">Verifica los campos</label>
-                )}
-              </fieldset>
-              <fieldset>
-                {emailIsTaken && (
-                  <label className="text-red-600">
-                    Email inválido o en uso
-                  </label>
-                )}
-              </fieldset>
-              <button className="btn">Registrarte</button>
-            </form>
+            {successRegistration ? (
+              <h2 className="form-title">Registro Exitoso!</h2>
+            ) : (
+              <h2 className="form-title">Registro</h2>
+            )}
+            {successRegistration ? (
+              <Link to={"/login"}>
+                <button className="btn">Iniciar sesión</button>
+              </Link>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <fieldset>
+                  <label htmlFor="name">Nombre</label>
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                  />
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={email}
+                  />
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="password">Contraseña</label>
+                  <input
+                    onChange={handleChange}
+                    type="password"
+                    id="password"
+                    placeholder="6+ caracteres"
+                    name="password"
+                    value={password}
+                  />
+                </fieldset>
+                <fieldset>
+                  {registerHasError && (
+                    <label className="text-red-600">Verifica los campos</label>
+                  )}
+                </fieldset>
+                <fieldset>
+                  {emailIsTaken && (
+                    <label className="text-red-600">
+                      Email inválido o en uso
+                    </label>
+                  )}
+                </fieldset>
+                <button className="btn" disabled={!name || !email || !password}>
+                  Registrarte
+                </button>
+              </form>
+            )}
           </div>
         </main>
       </section>
