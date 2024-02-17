428f26580f10f5e36f2fd9c4e09007cd38adbe01
Amane Misa <mansion.pe@gmail.com>
2024-02-17 13:22:28 -0500

    change shimmer skeleton to tailwind skeleton and remove unused package

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,7 +1,6 @@
 import { Badge } from "./icons";
 
 import { Link } from "react-router-dom";
-import { Skeleton } from "@mui/material";
 
 import "./styles/CharactersList.scss";
 
@@ -11,27 +10,27 @@ export const CharactersList = ({ badges }) => {
       <main className="pb-16">
         {[...Array(10)].map((e, i) => (
           <div className="character" key={i}>
-            <header>
-              <Skeleton variant="rect" animation="wave" height={355} />
-            </header>
-            <footer>
-              <div className="left-col">
-                <h2 className="name">
-                  <Skeleton variant="text" animation="wave" />
-                </h2>
-                <p className="desc">
-                  <Skeleton variant="text" animation="wave" />
-                </p>
+            <div
+              role="status"
+              class="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700"
+            >
+              <div class="flex items-center justify-center h-40 lg:h-60 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                <svg
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 16 20"
+                >
+                  <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                </svg>
               </div>
-              <div className="right-col">
-                <h2 className="price" alt="precio">
-                  <Skeleton variant="text" animation="wave" />
-                </h2>
-                <p className="time">
-                  <Skeleton variant="text" animation="wave" />
-                </p>
-              </div>
-            </footer>
+              <div class="h-2 lg:h-3 bg-gray-200 rounded-full dark:bg-gray-700 mb-2"></div>
+              <div class="h-2 lg:h-3 bg-gray-200 rounded-full dark:bg-gray-700 mb-2"></div>
+              <div class="h-2 lg:h-3 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              <span class="sr-only">Loading...</span>
+            </div>
           </div>
         ))}
       </main>
@@ -50,73 +49,69 @@ export const CharactersList = ({ badges }) => {
             }
           >
             <div className="character">
-              <header>
-                <img
-                  className="character_img"
-                  src={
-                    badge.images[0].slice(0, 48) +
-                    "e_improve/w_240,h_355,c_fill,f_auto" +
-                    badge.images[0].slice(47)
-                  }
-                  srcSet={
-                    badge.images[0].slice(0, 48) +
-                    "e_improve/w_140,h_190,c_fill,f_auto" +
-                    badge.images[0].slice(47) +
-                    " 140w," +
-                    badge.images[0].slice(0, 48) +
-                    "e_improve/w_240,h_355,c_fill,f_auto" +
-                    badge.images[0].slice(47) +
-                    " 240w,"
-                  }
-                  sizes="(max-width: 601px) 140px, 240px"
-                  alt=""
-                  height={355}
-                  width={240}
-                  loading={index > 5 ? "lazy" : "eager"}
-                  fetchpriority={index <= 5 ? "high" : "low"}
-                />
-              </header>
+              <img
+                className="character_img"
+                src={
+                  badge.images[0].slice(0, 48) +
+                  "e_improve/w_240,h_355,c_fill,f_auto" +
+                  badge.images[0].slice(47)
+                }
+                srcSet={
+                  badge.images[0].slice(0, 48) +
+                  "e_improve/w_140,h_190,c_fill,f_auto" +
+                  badge.images[0].slice(47) +
+                  " 140w," +
+                  badge.images[0].slice(0, 48) +
+                  "e_improve/w_240,h_355,c_fill,f_auto" +
+                  badge.images[0].slice(47) +
+                  " 240w,"
+                }
+                sizes="(max-width: 601px) 140px, 240px"
+                alt=""
+                height={355}
+                width={240}
+                loading={index > 5 ? "lazy" : "eager"}
+                fetchpriority={index <= 5 ? "high" : "low"}
+              />
               <footer>
-                <div className="left-col">
-                  <div className="name_wrapper">
-                    <h2 className="name">{badge.name}</h2>
-                    {badge.verified && <Badge />}
-                  </div>
-                  <p className="desc">
-                    {badge.rate_1h ? "S/" : null} {badge.rate_1h} ·{" "}
-                    {badge.nationality.label}
-                  </p>
-                  <div className="name_wrapper">
-                    {badge?.districtValue?.length > 0 && (
-                      <>
-                        <svg
-                          fill="#fff"
-                          version="1.1"
-                          id="Layer_1"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="18px"
-                          height="18px"
-                          viewBox="595 796 200 200"
-                          enable-background="new 595 796 200 200"
-                        >
-                          <g id="SVGRepo_bgCarrier" stroke-width="0" />
-                          <g
-                            id="SVGRepo_tracerCarrier"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                          <g id="SVGRepo_iconCarrier">
-                            <g>
-                              <path d="M700.539,993.184c-1.29,1.77-3.349,2.816-5.539,2.816c-2.191,0-4.249-1.047-5.539-2.817l-55.964-76.807 c-9.428-13.026-14.404-28.398-14.404-44.47C619.093,830.051,653.145,796,695,796s75.907,34.051,75.907,75.906 c0,16.087-4.971,31.46-14.375,44.455L700.539,993.184z M695,808.734c-34.834,0-63.174,28.339-63.174,63.172 c0,13.373,4.141,26.163,11.976,36.987l51.199,70.271l51.228-70.284c7.81-10.793,11.945-23.583,11.945-36.973 C758.174,837.073,729.834,808.734,695,808.734z M695,906.293c-18.96,0-34.386-15.425-34.386-34.387 c0-18.959,15.426-34.385,34.386-34.385c18.961,0,34.388,15.425,34.388,34.385C729.388,890.868,713.961,906.293,695,906.293z M695,850.255c-11.939,0-21.652,9.712-21.652,21.651c0,11.94,9.713,21.654,21.652,21.654c11.94,0,21.654-9.714,21.654-21.654 C716.654,859.967,706.94,850.255,695,850.255z" />
-                            </g>
+                <div className="name_wrapper">
+                  <h2 className="name">{badge.name}</h2>
+                  {badge.verified && <Badge />}
+                </div>
+                <div className="desc">
+                  {badge.rate_1h ? "S/" : null} {badge.rate_1h} ·{" "}
+                  {badge.nationality.label}
+                </div>
+                <div className="name_wrapper">
+                  {badge?.districtValue?.length > 0 && (
+                    <>
+                      <svg
+                        fill="#fff"
+                        version="1.1"
+                        id="Layer_1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="18px"
+                        height="18px"
+                        viewBox="595 796 200 200"
+                        enable-background="new 595 796 200 200"
+                      >
+                        <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                        <g
+                          id="SVGRepo_tracerCarrier"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                        <g id="SVGRepo_iconCarrier">
+                          <g>
+                            <path d="M700.539,993.184c-1.29,1.77-3.349,2.816-5.539,2.816c-2.191,0-4.249-1.047-5.539-2.817l-55.964-76.807 c-9.428-13.026-14.404-28.398-14.404-44.47C619.093,830.051,653.145,796,695,796s75.907,34.051,75.907,75.906 c0,16.087-4.971,31.46-14.375,44.455L700.539,993.184z M695,808.734c-34.834,0-63.174,28.339-63.174,63.172 c0,13.373,4.141,26.163,11.976,36.987l51.199,70.271l51.228-70.284c7.81-10.793,11.945-23.583,11.945-36.973 C758.174,837.073,729.834,808.734,695,808.734z M695,906.293c-18.96,0-34.386-15.425-34.386-34.387 c0-18.959,15.426-34.385,34.386-34.385c18.961,0,34.388,15.425,34.388,34.385C729.388,890.868,713.961,906.293,695,906.293z M695,850.255c-11.939,0-21.652,9.712-21.652,21.651c0,11.94,9.713,21.654,21.652,21.654c11.94,0,21.654-9.714,21.654-21.654 C716.654,859.967,706.94,850.255,695,850.255z" />
                           </g>
-                        </svg>
-                        <p className="desc ml-1">
-                          {badge?.districtValue[0]?.nombre_ubigeo || ""}
-                        </p>
-                      </>
-                    )}
-                  </div>
+                        </g>
+                      </svg>
+                      <p className="desc ml-1">
+                        {badge?.districtValue[0]?.nombre_ubigeo || ""}
+                      </p>
+                    </>
+                  )}
                 </div>
               </footer>
             </div>
